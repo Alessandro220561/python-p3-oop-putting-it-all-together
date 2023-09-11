115e187b2042b58cbc4b24071547706b4f5f1b41
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-09-11 15:42:35 -0400

    lab complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,58 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
+# class Book:
+#     def __init__(self, title, page_count):
+#         # if self.valid_page_count(page_count):
+#         #     self.page_count = page_count
+#         #     self.title = title
+#         # else:
+#         #     print("page_count must be an integer")
+#         if isinstance(page_count, int):
+#             self.title = title
+#             self.page_count = page_count
+#         else:
+#             raise ValueError("page_count must be an integer")      
+
+#     def turn_page(self):
+#         print("Flipping the page...wow, you read fast!")
     
-        
+#     # def valid_page_count(self, page_count):
+#     #     if type(page_count) == int:
+#     #         self.page_count = page_count
+
+# class Book:
+#     def __init__(self, title, page_count):
+#         if isinstance(page_count, int):
+#             self.title = title
+#             self.page_count = page_count
+#         else:
+#             try:
+#                 int(page_count)  # Try to convert page_count to an integer
+#             except ValueError:
+#                 print("page_count must be an integer")
+#             else:
+#                 self.title = title
+#                 self.page_count = int(page_count)
+
+#     def turn_page(self):
+#         print("Flipping the page...wow, you read fast!")
+
+class Book:
+    
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
